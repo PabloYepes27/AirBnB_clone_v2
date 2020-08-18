ef32242b22096ccd8ec3354006f1da2f8c47a2e2
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-08-17 22:45:17 -0500

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,12 @@ from fabric.api import local
 from datetime import datetime
 from os import path
 
+
 def do_pack():
     """[summary]"""
     local('mkdir -p versions')
-    tar_dir = local("tar -czvf versions/web_static_{}.tgz web_static/".format((datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))), capture=True)
+    tar_dir = local("tar -czvf versions/web_static_{}.tgz web_static/".format((
+        datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))), capture=True)
 
     if path.exists(tar_dir):
         return tar_dir
