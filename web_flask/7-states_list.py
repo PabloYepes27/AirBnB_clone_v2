e9c41170402edc620285179f8886c700604e68f9
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-09-02 17:15:33 -0500

    Task 8 - List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application
+and would be listening on 0.0.0.0, port 5000"""
+from flask import Flask, request, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = list(storage.all(State).values())
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def remove_session(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
